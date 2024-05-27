06d3884fea04ca2a6eae3e3195453f6bfbef5c40
Uzma Shaheen <160238653+ushaheen0000@users.noreply.github.com>
2024-05-27 03:00:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function startGame() {
+    console.log("Welcome to Adventure Game");
+    const name = await inquirer.prompt({
+        name: "name",
+        type: "input",
+        message: "What is your name?"
+    });
+    console.log(`Hello , ${name.name}`);
+    // start the Adventure
+    await startAdventure();
+}
+async function startAdventure() {
+    const { choice } = await inquirer.prompt({
+        name: "choice",
+        type: "list",
+        choices: ["Explore the forest", "Enter the Cave", "Quit"],
+        message: "What do you want to do?"
+    });
+    switch (choice) {
+        case "Explore the forest":
+            console.log("you venture into the forest.");
+            break;
+        case "Enter the Cave":
+            console.log("You entered the Cave.");
+            break;
+        case "Quit":
+            console.log("Thanks for playing Game");
+            return;
+    }
+    // continue the Adventure
+    await startAdventure();
+}
+// Start the Game
+startGame();
